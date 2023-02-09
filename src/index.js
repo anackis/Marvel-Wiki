d41d1218967306f239000c4a1f9ff1509e11aec9
Alex <rengarsanj@gmail.com>
2023-02-09 14:42:01 +0200

    Save point before critical changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
+// import MarvelService from './services/MarvelService';
 import './style/style.scss';
+
+// const marvelService = new MarvelService();
+// marvelService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name )));
+// marvelService.getCharacter(1011052).then(res => res.data.results);
+// console.log(marvelService.getCharacter(1011052).then(res => res.data.results));
+
 
 ReactDOM.render(
   <React.StrictMode>
